02c09cfd4e8e70645d26b1f91c916527eec35a7e
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-10-16 12:04:57 +0200

    Adding autogenerated equals and hashCode functions for dataclass with ByteArray

--- src/main/kotlin/no/nav/syfo/document/api/v1/Document.kt
+++ src/main/kotlin/no/nav/syfo/document/api/v1/Document.kt
@@ -26,6 +26,34 @@ data class Document(
             dialogId = null,
         )
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Document
+
+        if (documentId != other.documentId) return false
+        if (type != other.type) return false
+        if (!content.contentEquals(other.content)) return false
+        if (contentType != other.contentType) return false
+        if (orgnumber != other.orgnumber) return false
+        if (dialogTitle != other.dialogTitle) return false
+        if (dialogSummary != other.dialogSummary) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = documentId.hashCode()
+        result = 31 * result + type.hashCode()
+        result = 31 * result + content.contentHashCode()
+        result = 31 * result + contentType.hashCode()
+        result = 31 * result + orgnumber.hashCode()
+        result = 31 * result + dialogTitle.hashCode()
+        result = 31 * result + dialogSummary.hashCode()
+        return result
+    }
 }
 
 enum class DocumentType {
