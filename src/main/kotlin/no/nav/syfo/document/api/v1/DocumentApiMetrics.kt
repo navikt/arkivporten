db36497ade4462f101a20365698033029093b579
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-12-12 13:25:13 +0100

    Typo

--- src/main/kotlin/no/nav/syfo/document/api/v1/DocumentApiMetrics.kt
+++ src/main/kotlin/no/nav/syfo/document/api/v1/DocumentApiMetrics.kt
@@ -9,6 +9,7 @@ val COUNT_DOCUMENT_RECIEVED: Counter = Counter.builder(DOCUMENT_RECIEVED)
     .description("Counts the number of documents recieved")
     .register(METRICS_REGISTRY)
 
+
 const val DOCUMENTS_READ_BY_EXTERNAL_IDPORTENUSER = "${METRICS_NS}_documents_read_by_external_idportenuser"
 val COUNT_DOCUMENTS_READ_BY_EXTERNAL_IDPORTENUSER: Counter = Counter.builder(DOCUMENTS_READ_BY_EXTERNAL_IDPORTENUSER)
     .description("Counts the number of documents read by external idporten user")
@@ -26,5 +27,5 @@ val COUNT_DOCUMENTS_REREAD_BY_EXTERNAL_IDPORTENUSER: Counter = Counter.builder(D
 
 const val DOCUMENTS_REREAD_BY_EXTERNAL_SYSTEMUSER = "${METRICS_NS}_documents_reread_by_external_systemuser"
 val COUNT_DOCUMENTS_REREAD_BY_EXTERNAL_SYSTEMUSER: Counter = Counter.builder(DOCUMENTS_REREAD_BY_EXTERNAL_SYSTEMUSER)
-    .description("Counts the number of documents reread by external sustem user")
+    .description("Counts the number of documents reread by external system user")
     .register(METRICS_REGISTRY)
